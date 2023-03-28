011138e513071124530a322a7d1f1ee39dfed408
IMPWNG <matisse.ach+github@gmail.com>
2023-03-28 10:13:27 -0400

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,18 +13,20 @@ const Home: NextPage = () => {
   return (
     <Grid container justifyContent="center" alignItems="center" sx={{ height: '80vh' }}>
         {!session ? (
-          <Grid container justifyContent="center" alignItems="center" spacing={2}>
-            <Grid item>
-              <Typography variant="h4" align="center">
-                You are not signed in
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="body1" align="center">
-                Please sign in to access this page.
-              </Typography>
-            </Grid>
-          </Grid>
+ 
+
+              <><div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 text-center sm:py-0">
+          <div className="flex flex-col items-center justify-center w-full px-4 pt-16 pb-12 text-center bg-white border-2 border-gray-300 rounded-md shadow-md sm:px-6 sm:pt-12 sm:pb-16 sm:shadow-lg sm:max-w-md sm:w-full sm:mx-auto sm:mt-0 sm:mb-0">
+            <div className="flex flex-col items-center justify-center w-full">
+              <p className="text-2xl font-bold text-gray-900">You are not signed in</p>
+
+
+            </div>
+          </div>
+
+
+        </div></>
+
         ) : (
           <GeneralForm />
         )}
